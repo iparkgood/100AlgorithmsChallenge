04655ded4eb3a550742d579856ffd1aa77499a2a
iparkgood <zuzu8723@gmail.com>
2021-04-20 11:38:16 -0400

    solved add

--- add/add.ts
+++ add/add.ts
@@ -1,12 +1,12 @@
-export function add(param1: number, param2: number): number {
+import { textSpanOverlap, TypeFormatFlags } from "typescript";
 
+export function add(param1: number, param2: number): number {
+  return param1 + param2;
 }
 
 export function add2(...param1: number[]): number {
-
+  const result = param1.reduce((total, num) => {
+    return (total += num);
+  }, 0);
+  return result;
 }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
-
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
