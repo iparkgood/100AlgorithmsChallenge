2c5c720cf18f7aead64cc7af5ea1e70d2fc32030
iparkgood <zuzu8723@gmail.com>
2021-04-21 11:16:58 -0400

    solved allLongestStrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,18 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+  let longestLeng = 0;
+  let longestStrs = [];
 
+  inputArray.forEach((input) => {
+    longestLeng = input.length < longestLeng ? longestLeng : input.length;
+  });
+
+  inputArray.forEach((input) => {
+    if (longestLeng === input.length) {
+      longestStrs.push(input);
+    }
+  });
+
+  return longestStrs;
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(["aba", "aaaa", "addc", "vcd", "aba"]));
