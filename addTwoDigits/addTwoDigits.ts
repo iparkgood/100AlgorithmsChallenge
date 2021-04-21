e244e7e2c9895b8fdda280c39f8648e0976432ca
iparkgood <zuzu8723@gmail.com>
2021-04-21 09:59:20 -0400

    solved addTwoDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,9 @@
 export function addTwoDigits(n: any): number {
+    const numArr = n.toString().split('');
 
+    return numArr.reduce((acc, num) => {
+        return acc+= parseInt(num);
+    }, 0);
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
