bf3148bb718ccc96c29f53f232f8895d9d6b2d7a
iparkgood <zuzu8723@gmail.com>
2021-04-22 15:36:45 -0400

    solved alphabetSubSequence

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,26 @@
 export function alphabetSubsequence(s: string): boolean {
+  const chars: string[] = s.split("");
+  let charValues: number[] = [];
 
+  chars.forEach((c: string) => {
+    charValues.push(c.charCodeAt(0));
+  });
+
+  if (new Set(charValues).size !== charValues.length) {
+    return false;
+  }
+
+  for (let i = 0; i < charValues.length - 1; i++) {
+    if (charValues[i] >= charValues[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence("zab"));
+console.log(alphabetSubsequence("effg"));
+console.log(alphabetSubsequence("cdce"));
+console.log(alphabetSubsequence("ace"));
+console.log(alphabetSubsequence("bxz"));
