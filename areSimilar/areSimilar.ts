a80974137d5f1cdefab2f942993c31bbcdecf036
iparkgood <zuzu8723@gmail.com>
2021-04-23 09:29:32 -0400

    solved areSimilar

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,7 +1,23 @@
 export function areSimilar(a: number[], b: number[]): boolean {
+  let c: number[] = [];
+  let d: number[] = [];
 
+  if (a.toString() === b.toString()) {
+    return true;
+  }
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      c.push(a[i]);
+      d.push(b[i]);
+    }
+  }
+
+  return c.length === 2 && c.toString() === d.reverse().toString()
+    ? true
+    : false;
 }
 
-// console.log(areSimilar([1, 2, 3], [1, 2, 3]));
-// console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-// console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));
