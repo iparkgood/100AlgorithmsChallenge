e461196c93fd43971fe6df982d353420dafcae30
iparkgood <zuzu8723@gmail.com>
2021-04-23 08:52:45 -0400

    solved alternatingSums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,12 @@
 export function alternatingSums(a: number[]): number[] {
+  let evenSum = 0;
+  let oddSum = 0;
 
+  a.forEach((el, idx) => {
+    idx % 2 === 0 ? (evenSum += el) : (oddSum += el);
+  });
+
+  return [evenSum, oddSum];
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
