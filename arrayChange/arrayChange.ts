4ca14f77686f9d3cb2a7202ac4c4c304f2108566
iparkgood <zuzu8723@gmail.com>
2021-04-26 14:47:13 -0400

    solved arrayChange

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,16 @@
 export function arrayChange(inputArray: number[]): number {
+  let count = 0;
 
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    if (inputArray[i] >= inputArray[i + 1]) {
+      const diff = inputArray[i] + 1 - inputArray[i + 1];
+      inputArray[i + 1] = inputArray[i + 1] + diff;
+      count += diff;
+    }
+  }
+
+  return count;
 }
 
-// console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([4, 2, 4]));
