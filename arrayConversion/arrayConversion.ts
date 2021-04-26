5e58313e9591381abc7de73f74ffcd8e0eb2e606
iparkgood <zuzu8723@gmail.com>
2021-04-26 15:30:01 -0400

    solved arrayConversion

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,28 @@
 export function arrayConversion(inputArray: number[]): number {
+  let isOdd = true;
 
+  while (inputArray.length !== 1) {
+    inputArray = sumProduct(inputArray, isOdd);
+    isOdd = !isOdd;
+  }
+
+  return inputArray[0];
 }
 
-// console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+function sumProduct(inputArray: number[], isOdd: boolean): number[] {
+  let result: number[] = [];
+
+  if (isOdd) {
+    for (let i = 0; i < inputArray.length; i += 2) {
+      result.push(inputArray[i] + inputArray[i + 1]);
+    }
+  } else {
+    for (let i = 0; i < inputArray.length; i += 2) {
+      result.push(inputArray[i] * inputArray[i + 1]);
+    }
+  }
+
+  return result;
+}
+
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
