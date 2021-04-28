6e8c6cee53617377194ea1dce2d11ed5b70a96a2
iparkgood <zuzu8723@gmail.com>
2021-04-28 12:57:38 -0400

    solved avoidObstacles

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,11 @@
 export function avoidObstacles(inputArray: number[]): number {
+  const largestVal = Math.max(...inputArray);
 
+  for (let i = 1; i <= largestVal + 1; i++) {
+    if (inputArray.every((input) => input % i !== 0)) {
+      return i;
+    }
+  }
 }
 
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
+console.log(avoidObstacles([5, 3, 6, 9, 7]));
