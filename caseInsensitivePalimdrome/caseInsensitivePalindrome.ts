cbc474b99f2993a50220ec9c13b156b313ed1c4b
iparkgood <zuzu8723@gmail.com>
2021-04-29 13:29:49 -0400

    solved caseInsensitivePalimdrome

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,9 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+  return (
+    inputString.toLowerCase() ===
+    [...inputString.toLowerCase()].reverse().join("")
+  );
 }
 
-// console.log(isCaseInsensitivePalindrome('AaBaa'));
-// console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome("AaBaa"));
+console.log(isCaseInsensitivePalindrome("abac"));
