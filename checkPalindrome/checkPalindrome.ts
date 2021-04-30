926c5dddd80399626f7599617059445cc6212a8d
iparkgood <zuzu8723@gmail.com>
2021-04-30 16:01:11 -0400

    solved checkPalindrome

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,7 @@
 export function checkPalindrome(inputString: string): boolean {
-
+    return inputString.toLowerCase() === inputString.toLowerCase().split("").reverse().join("");
 }
 
-// console.log(checkPalindrome('aabaa'));
-// console.log(checkPalindrome('abac'));
-// console.log(checkPalindrome('a'));
+console.log(checkPalindrome('aabaa')); //true
+console.log(checkPalindrome('abac')); //false
+console.log(checkPalindrome('a')); //true
