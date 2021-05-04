bd1bb81f4c0cd2f50bce4e0ad9f13a3fbce988bb
iparkgood <zuzu8723@gmail.com>
2021-05-04 15:35:39 -0400

    solved chunkyMonkey

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,6 +1,13 @@
 export function chunkyMonkey(arr: any[], size: number): any[][] {
+  let count = 0;
+  const nested = [];
 
+  while (count < arr.length) {
+    nested.push(arr.slice(count, (count += size)));
+  }
+
+  return nested;
 }
 
-// console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
-// console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
+console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
