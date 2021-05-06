3a8d3caa7961bcf401fa6fdc2c0704c9446c6f08
iparkgood <zuzu8723@gmail.com>
2021-05-06 11:20:30 -0400

    solved companyBotStrategy

--- companyBotStrategy/companyBotStrategy.ts
+++ companyBotStrategy/companyBotStrategy.ts
@@ -1,7 +1,37 @@
 export function companyBotStrategy(trainingData: number[][]): number {
+  let sum = 0;
+  let count = 0;
 
+  trainingData.forEach(([answerTime, correctness]) => {
+    if (correctness === 1) {
+      sum += answerTime;
+      count += correctness;
+    }
+  });
+
+  return sum / count || 0;
 }
 
-// console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
-// console.log(companyBotStrategy([[4, 1], [4, -1], [0, 0], [6, 1]]));
-// console.log(companyBotStrategy( [[4, -1], [0, 0], [5, -1]]));
+console.log(
+  companyBotStrategy([
+    [3, 1],
+    [6, 1],
+    [4, 1],
+    [5, 1],
+  ])
+);
+console.log(
+  companyBotStrategy([
+    [4, 1],
+    [4, -1],
+    [0, 0],
+    [6, 1],
+  ])
+);
+console.log(
+  companyBotStrategy([
+    [4, -1],
+    [0, 0],
+    [5, -1],
+  ])
+);
