4c396939e9bfa70d827bff53c20166885c172e3b
iparkgood <zuzu8723@gmail.com>
2021-05-07 17:01:10 -0400

    solved confirmEnding

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,8 @@
 export function confirmEnding(str: string, target: string) {
-
+  // return str.endsWith(target);
+  // return str.substr(-target.length,target.length) === target;
+  return str.substr(-target.length) === target;
 }
 
-// console.log(confirmEnding("Abstraction", "action"));
-// console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding("Abstraction", "action"));
+console.log(confirmEnding("Open sesame", "pen"));
