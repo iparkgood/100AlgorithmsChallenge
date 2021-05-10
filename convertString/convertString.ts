dbbf6cb781d4d7d5d4a9f201a94796bcc8afcba1
iparkgood <zuzu8723@gmail.com>
2021-05-10 10:46:30 -0400

    solved convertString

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -1,6 +1,17 @@
 export function convertString(s: string, t: string): boolean {
+  const sArr = [...s];
+  let tIdx = 0;
+  let word = "";
 
+  for (let i = 0; i < sArr.length; i++) {
+    if (sArr[i] === t[tIdx]) {
+      word = word.concat(sArr[i]);
+      tIdx++;
+    }
+  }
+
+  return word === t;
 }
 
-// console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
-// console.log(convertString('addbyca', 'abcd'));
+console.log(convertString("ceoydefthf5iyg5h5yts", "codefights")); //true
+console.log(convertString("addbyca", "abcd")); //false
