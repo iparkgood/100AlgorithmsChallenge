ce7ca5bf1257c2c5917721f6aca33e9defc3acc5
iparkgood <zuzu8723@gmail.com>
2021-05-12 12:29:15 -0400

    solved depositProfit

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,17 @@
-export function depositProfit(deposit: number, rate: number, threshold: number): number {
+export function depositProfit(
+  deposit: number,
+  rate: number,
+  threshold: number
+): number {
+  let year = 0;
+  let sum = deposit;
 
+  while (sum < threshold) {
+    sum = sum + sum * rate / 100;
+    year++;
+  }
+
+  return year;
 }
 
-// console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 20, 170)); //3
