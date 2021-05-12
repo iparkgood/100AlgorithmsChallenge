908d4beac1428d957630b75a9b41518df06032cc
iparkgood <zuzu8723@gmail.com>
2021-05-12 12:51:42 -0400

    solved differentSymbolIsNaive

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,11 @@
 export function differentSymbolsNaive(s: string): number {
+  let diff = [];
 
+  [...s].forEach((char) => {
+    if (!diff.includes(char)) diff.push(char);
+  });
+
+  return diff.length;
 }
 
-// console.log(differentSymbolsNaive('cabca'));
+console.log(differentSymbolsNaive("cabca"));
