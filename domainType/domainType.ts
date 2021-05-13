456b674c1e03d1ddebf995c7f5a06522d38c0938
iparkgood <zuzu8723@gmail.com>
2021-05-13 13:20:42 -0400

    solved domainType

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,16 @@
 export function domainType(domains: string[]): string[] {
-
+  return domains.map((domain) => {
+    const words = domain.split(".");
+    return words[words.length - 1] === "org"
+      ? "organization"
+      : words[words.length - 1] === "com"
+      ? "commercial"
+      : words[words.length - 1] === "net"
+      ? "network"
+      : "information";
+  });
 }
 
-// console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(
+  domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"])
+);
