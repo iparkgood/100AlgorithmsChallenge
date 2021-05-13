dd6c80a7f022df1e85d1a1391076e2c1c48a8856
iparkgood <zuzu8723@gmail.com>
2021-05-13 13:05:13 -0400

    solved digitDegree

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,7 +1,19 @@
 export function digitDegree(n: number): number {
+  const nums = [...n.toString()].map((num) => +num);
 
+  if (nums.length === 1) return 0;
+
+  let sum = nums.reduce((acc, num) => acc + num);
+  let count = 1;
+
+  while (sum.toString().length !== 1) {
+    sum = [...sum.toString()].reduce((acc, num) => acc + +num, 0);
+    count++;
+  }
+
+  return count;
 }
 
-// console.log(digitDegree(5));
-// console.log(digitDegree(10));
-// console.log(digitDegree(91));
+console.log(digitDegree(5));
+console.log(digitDegree(10));
+console.log(digitDegree(911));
