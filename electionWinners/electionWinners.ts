d426b4568e25e10da155b9d2ea3b9dc1b8ece1a4
iparkgood <zuzu8723@gmail.com>
2021-05-14 14:00:25 -0400

    solved electionWinners

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,17 @@
 export function electionsWinners(votes: number[], k: number): number {
+  let count = 0;
+  const max = Math.max(...votes);
+  votes.sort((a, b) => b - a);
 
+  if (votes[0] !== votes[1] && k === 0) {
+    return 1;
+  }
+
+  votes.forEach((vote) => {
+    if (max - vote < k) count++;
+  });
+
+  return count;
 }
 
-// console.log(electionsWinners([2, 3, 5, 2], 3));
+console.log(electionsWinners([2, 3, 5, 2], 3)); //2
