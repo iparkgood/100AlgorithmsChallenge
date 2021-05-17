7080a60d3a61b63c9a4b5df1f9c2f36feae2a0d6
iparkgood <zuzu8723@gmail.com>
2021-05-17 15:41:01 -0400

    solved factorializeANumber

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,12 @@
 export function factorializeANumber(num: number): number {
+  let factorial = 1;
 
+  for (let i = num; i > 0; i--) {
+    factorial *= i;
+  }
+
+  return factorial;
 }
 
-// console.log(factorializeANumber(5));
-// console.log(factorializeANumber(10));
+console.log(factorializeANumber(5));
+console.log(factorializeANumber(10));
