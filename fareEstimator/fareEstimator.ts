ab8213b163f1a253f85dd80f3e375cccc44f7fc0
iparkgood <zuzu8723@gmail.com>
2021-05-18 09:54:42 -0400

    solved fareEstimator

--- fareEstimator/fareEstimator.ts
+++ fareEstimator/fareEstimator.ts
@@ -1,5 +1,21 @@
-export function fareEstimator(ride_time: number, ride_distance: number, cost_per_minute: number[], cost_per_mile: number[]): number[] {
+export function fareEstimator(
+  ride_time: number,
+  ride_distance: number,
+  cost_per_minute: number[],
+  cost_per_mile: number[]
+): number[] {
+  let fares = [];
 
+  for (let i = 0; i < cost_per_mile.length; i++) {
+    fares.push(
+      +(
+        cost_per_minute[i] * ride_time +
+        cost_per_mile[i] * ride_distance
+      ).toFixed(1)
+    );
+  }
+
+  return fares;
 }
 
-// console.log(fareEstimator(30, 7, [0.2, 0.35, 0.4, 0.45], [1.1, 1.8, 2.3, 3.5]));
+console.log(fareEstimator(30, 7, [0.2, 0.35, 0.4, 0.45], [1.1, 1.8, 2.3, 3.5]));
