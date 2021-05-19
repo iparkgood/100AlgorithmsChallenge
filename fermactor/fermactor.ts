e921160ba74ee844e363fdca7466a548706943b0
iparkgood <zuzu8723@gmail.com>
2021-05-19 10:43:11 -0400

    solved fermactor

--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,14 @@
 export function fermactor(n: number): number[] {
+  for (let i = 1; i < n; i++) {
+    for (let j = 1; j < i; j++) {
+      const total = i ** 2 - j ** 2;
+      //Math.pow(i, 2) - Math.pow(j, 2)
 
+      if (total === n) {
+        return [i, j];
+      }
+    }
+  }
 }
 
-// console.log(fermactor(15));
+console.log(fermactor(15));
