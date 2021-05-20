a91326c36cdbfd97fc2a75dc8a157b658ba3fd67
iparkgood <zuzu8723@gmail.com>
2021-05-20 10:50:07 -0400

    solved firstDigit

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,7 +1,7 @@
 export function firstDigit(inputString: string): string {
-
+  return inputString.match(/\d/g).toString();
 }
 
-// console.log(firstDigit('var_1__Int'));
-// console.log(firstDigit('q2q-q'));
-// console.log(firstDigit('0ss'));
+console.log(firstDigit("var_1__Int"));
+console.log(firstDigit("q2q-q"));
+console.log(firstDigit("0ss"));
