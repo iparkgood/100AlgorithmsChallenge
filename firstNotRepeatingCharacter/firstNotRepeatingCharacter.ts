4a5308c9bad2b2e38d8953983bb9d0a3e0177b4f
iparkgood <zuzu8723@gmail.com>
2021-05-20 12:32:24 -0400

    solved firstNotRepeatingCharacter

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,6 +1,12 @@
 export function firstNotRepeatingCharacter(s: string): string {
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+      return s[i];
+    }
+  }
 
+  return "_";
 }
 
-// console.log(firstNotRepeatingCharacter('abacabad'));
-// console.log(firstNotRepeatingCharacter('abacabaabacaba'));
+console.log(firstNotRepeatingCharacter("abacabad"));
+console.log(firstNotRepeatingCharacter("abacabaabacaba"));
