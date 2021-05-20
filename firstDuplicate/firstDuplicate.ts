025adbfbc1f81c075bad4e262a5ce9a0c03aae3d
iparkgood <zuzu8723@gmail.com>
2021-05-20 11:02:14 -0400

    solved firstDuplicate

--- firstDuplicate/firstDuplicate.ts
+++ firstDuplicate/firstDuplicate.ts
@@ -1,6 +1,16 @@
 export function firstDuplicate(a: number[]): number {
+  let dupes = {};
 
+  for (let i = 0; i < a.length; i++) {
+    if (dupes.hasOwnProperty(a[i])) {
+      return a[i];
+    } else {
+      dupes[a[i]] = 1;
+    }
+  }
+
+  return -1;
 }
 
-// console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
-// console.log(firstDuplicate([2, 4, 3, 5, 1]));
+console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
+console.log(firstDuplicate([2, 4, 3, 5, 1]));
