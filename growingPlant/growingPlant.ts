c9109ee5353113a08b47d27b4662f7f935b777f3
iparkgood <zuzu8723@gmail.com>
2021-05-23 11:40:17 -0400

    solved growingPlant

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,21 @@
-export function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
+export function growingPlant(
+  upSpeed: number,
+  downSpeed: number,
+  desiredHeight: number
+): number {
+  let days = 0;
+  let height = 0;
 
+  while (height < desiredHeight) {
+    days++;
+    height += upSpeed;
+
+    if (height < desiredHeight) {
+      height -= downSpeed;
+    }
+  }
+
+  return days;
 }
 
-// console.log(growingPlant(100, 10, 910))
+console.log(growingPlant(100, 10, 910));
