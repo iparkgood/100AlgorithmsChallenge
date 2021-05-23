9cd082eb12c4e36ec139c8c3e2a3b5748b167bc1
iparkgood <zuzu8723@gmail.com>
2021-05-23 11:29:21 -0400

    solved flattenArray

--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,6 +1,21 @@
 export function flattenArray(arr: any[]): any[] {
+  //   return [...arr.toString()].filter((el) => el.match(/[^","]/g));
+  let result = [];
 
+  flatten(arr);
+
+  function flatten(arr: any[]) {
+    arr.forEach((el) => {
+      if (Array.isArray(el)) {
+        return flatten(el);
+      } else {
+        result.push(el);
+      }
+    });
+  }
+
+  return result;
 }
 
-// console.log(flattenArray([[["a"]], [["b"]]]));
-// console.log(flattenArray([1, [2], [3, [[4]]]]));
+console.log(flattenArray([[["a"]], [["b"]]]));
+console.log(flattenArray([1, [2], [3, [[4]]]]));
