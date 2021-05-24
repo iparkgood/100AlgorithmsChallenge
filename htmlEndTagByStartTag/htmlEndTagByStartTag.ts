8dc1ab4041b3571b0bb6f8874c266cafab0fc3a7
iparkgood <zuzu8723@gmail.com>
2021-05-24 13:35:04 -0400

    solved htmlEndTagByStartTag

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,6 +1,6 @@
 export function htmlEndTagByStartTag(startTag: string): string {
-
+  return "</" + startTag.split(" ")[0].match(/\w+/g) + ">";
 }
 
-// console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-// console.log(htmlEndTagByStartTag('<i>'));
+console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
+console.log(htmlEndTagByStartTag("<i>"));
