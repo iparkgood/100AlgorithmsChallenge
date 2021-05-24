b11835e21205f38b0c30988ce3fd7a62fbc04a50
iparkgood <zuzu8723@gmail.com>
2021-05-24 12:55:36 -0400

    solved houseOfCats

--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,6 +1,11 @@
 export function houseOfCats(legs: number): number[] {
+  let howMany = [];
 
+  if (Math.floor(legs / 4) !== 0) howMany.push(Math.floor(legs / 4));
+  if (Math.floor(legs / 2) !== 0) howMany.push(Math.floor(legs / 2));
+
+  return howMany;
 }
 
-// console.log(houseOfCats(6));
-// console.log(houseOfCats(2));
+console.log(houseOfCats(6));
+console.log(houseOfCats(2));
