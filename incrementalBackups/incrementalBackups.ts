f34e4ae06b06268f6c9123465e9003e9150bc704
iparkgood <zuzu8723@gmail.com>
2021-05-25 18:28:48 -0400

    solved incrementalBackups

--- incrementalBackups/incrementalBackups.ts
+++ incrementalBackups/incrementalBackups.ts
@@ -1,11 +1,26 @@
-export function incrementalBackups(lastBackupTime: number, changes: number[][]): number[] {
+export function incrementalBackups(
+  lastBackupTime: number,
+  changes: number[][]
+): number[] {
+  let backups = [];
 
+  changes.forEach(([timestamp, id]) => {
+    if (timestamp > lastBackupTime && !backups.includes(id)) {
+      backups.push(id);
+    }
+  });
+
+  return backups.sort((a, b) => a - b);
 }
 
-// console.log(incrementalBackups(461620205, [[461620203, 1], 
-//     [461620204, 2], 
-//     [461620205, 6],
-//     [461620206, 5], 
-//     [461620207, 3], 
-//     [461620207, 5], 
-//     [461620208, 1]]));
+console.log(
+  incrementalBackups(461620205, [
+    [461620203, 1],
+    [461620204, 2],
+    [461620205, 6],
+    [461620206, 5],
+    [461620207, 3],
+    [461620207, 5],
+    [461620208, 1],
+  ])
+);
