5f626d1ed2410d364b28864b5cb2a026ec0c81b0
iparkgood <zuzu8723@gmail.com>
2021-05-25 18:49:02 -0400

    refactored alphabeticShift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,39 +1,10 @@
 export function alphabeticShift(inputString: string): string {
-  const inputArr = inputString.split("");
-  const alphabets: object = {
-    a: "b",
-    b: "c",
-    c: "d",
-    d: "e",
-    e: "f",
-    f: "g",
-    g: "h",
-    h: "i",
-    i: "j",
-    j: "k",
-    k: "l",
-    l: "m",
-    m: "n",
-    n: "o",
-    o: "p",
-    p: "q",
-    q: "r",
-    r: "s",
-    s: "t",
-    t: "u",
-    u: "v",
-    v: "w",
-    w: "x",
-    x: "y",
-    y: "z",
-    z: "a",
-  };
-
-  return inputArr
+  return [...inputString]
     .map((input) => {
-      return input.replace(input, alphabets[input]);
+      return String.fromCharCode(input.charCodeAt(0) + 1);
     })
-    .join("");
+    .join("")
+    .replace(/\W/, "a");
 }
 
 console.log(alphabeticShift("crazy"));
