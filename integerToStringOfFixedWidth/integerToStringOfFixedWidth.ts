5bcd57e492ca623b10b20d9767c2a4f10e16a8d0
iparkgood <zuzu8723@gmail.com>
2021-05-26 10:45:38 -0400

    solved integerToStringOfFixedWidth

--- integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
@@ -1,7 +1,11 @@
 export function integerToStringOfFixedWidth(number: number, width: number): string {
+    if(number.toString().length < width) {
+        return "0".repeat(width - number.toString().length) + number;
+    }
 
+    return number.toString().slice(-width);
 }
 
-// console.log(integerToStringOfFixedWidth(1234, 2));
-// console.log(integerToStringOfFixedWidth(1234, 4));
-// console.log(integerToStringOfFixedWidth(1234, 5));
+console.log(integerToStringOfFixedWidth(1234, 2));
+console.log(integerToStringOfFixedWidth(1234, 4));
+console.log(integerToStringOfFixedWidth(1234, 5));
