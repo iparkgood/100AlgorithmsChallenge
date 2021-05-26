37feda6270814d249d6703939145a524d1065cf5
iparkgood <zuzu8723@gmail.com>
2021-05-26 11:02:15 -0400

    solved isLucky

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,6 +1,10 @@
 export function isLucky(n: number): boolean {
+  const numbers = [...n.toString()];
+  const first = numbers.slice(0, numbers.length / 2).reduce((sum, f) => sum + +f, 0);
+  const second = numbers.slice(-numbers.length / 2).reduce((sum, s) => sum + +s, 0)
 
+  return first === second;
 }
 
-// console.log(isLucky(1230));
-// console.log(isLucky(239017));
+console.log(isLucky(1230));
+console.log(isLucky(239017));
