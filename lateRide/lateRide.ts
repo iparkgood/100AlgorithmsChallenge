8e4b48bf557f4ea4872d3bc988a583a0ec76ff71
iparkgood <zuzu8723@gmail.com>
2021-05-27 12:44:58 -0400

    solved lateRide

--- lateRide/lateRide.ts
+++ lateRide/lateRide.ts
@@ -1,6 +1,9 @@
 export function lateRide(n: number): number {
+  const min = Math.floor(n / 60);
+  const sec = n - min * 60;
 
+  return [...(min.toString() + sec.toString())].reduce((sum, t) => sum + +t, 0);
 }
 
-// console.log(lateRide(240));
-// console.log(lateRide(808));
+console.log(lateRide(240));
+console.log(lateRide(808));
