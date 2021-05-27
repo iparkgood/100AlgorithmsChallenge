73babb5846a2fb93e414a20e4d4e783d0f2fc310
iparkgood <zuzu8723@gmail.com>
2021-05-27 12:24:16 -0400

    solved isTandemRepeat

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,10 @@
 export function isTandemRepeat(inputString: string): boolean {
-
+  return (
+    inputString.slice(0, Math.floor(inputString.length / 2)) ===
+    inputString.slice(Math.floor(-inputString.length / 2))
+  );
 }
 
-// console.log(isTandemRepeat('tandemtandem'))
-// console.log(isTandemRepeat('qqq'))
-// console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat("tandemtandem"));
+console.log(isTandemRepeat("qqq"));
+console.log(isTandemRepeat("2w2ww"));
