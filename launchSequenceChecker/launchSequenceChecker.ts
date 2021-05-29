f334a7f2395cba941b1d9368fb6e36bb4f4480b5
iparkgood <zuzu8723@gmail.com>
2021-05-29 19:49:29 -0400

    sovled launchSequenceChecker

--- launchSequenceChecker/launchSequenceChecker.ts
+++ launchSequenceChecker/launchSequenceChecker.ts
@@ -1,6 +1,32 @@
-export function launchSequenceChecker(systemNames: string[], stepNumbers: number[]): boolean {
+export function launchSequenceChecker(
+  systemNames: string[],
+  stepNumbers: number[]
+): boolean {
+  let systemsSteps = {};
 
+  for (let i = 0; i < systemNames.length; i++) {
+    if (systemsSteps.hasOwnProperty(systemNames[i])) {
+      if (systemsSteps[systemNames[i]] >= stepNumbers[i]) {
+        return false;
+      }
+      systemsSteps[systemNames[i]].push(stepNumbers[i]);
+    } else {
+      systemsSteps[systemNames[i]] = [stepNumbers[i]];
+    }
+  }
+
+  return true;
 }
 
-// console.log(launchSequenceChecker(["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"], [1, 10, 11, 2, 12, 111]));
-// console.log(launchSequenceChecker(["stage_1", "stage_1", "stage_2", "dragon"], [2, 1, 12, 111]));
+console.log(
+  launchSequenceChecker(
+    ["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"],
+    [1, 10, 11, 2, 12, 111]
+  )
+);
+console.log(
+  launchSequenceChecker(
+    ["stage_1", "stage_1", "stage_2", "dragon"],
+    [2, 1, 12, 111]
+  )
+);
