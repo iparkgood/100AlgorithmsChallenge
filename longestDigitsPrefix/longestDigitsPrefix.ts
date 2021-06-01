fc6cf6ce59b0c3c1bed9a762bd03e47c35bde99a
iparkgood <zuzu8723@gmail.com>
2021-06-01 11:13:10 -0400

    sovled longestDigitsPrefix

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,20 @@
 export function longestDigitsPrefix(inputString: string): string {
+  let digits = "";
+  let max = "";
 
+  for (let i = 0; i < inputString.length; i++) {
+    if (Number.isInteger(+inputString[i])) {
+      digits += inputString[i];
+      if (!Number.isInteger(+inputString[i + 1])) {
+        if (max.length < digits.length) {
+          max = digits;
+          digits = "";
+        }
+      }
+    }
+  }
+
+  return max;
 }
 
-// console.log(longestDigitsPrefix('123aa1'));
+console.log(longestDigitsPrefix("123aa4567"));
