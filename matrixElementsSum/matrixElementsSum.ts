0629512c2188e5d1df98e0242892da3dda1a66d4
iparkgood <zuzu8723@gmail.com>
2021-06-16 12:19:34 -0400

    solved matrixElementsSum

--- matrixElementsSum/matrixElementsSum.ts
+++ matrixElementsSum/matrixElementsSum.ts
@@ -1,9 +1,26 @@
 export function matrixElementsSum(matrix: any[][]): number {
+  let count: number = 0;
+  let colIdx: number[] = [];
 
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        colIdx.push(j);
+      } else {
+        if (!colIdx.includes(j)) {
+          count += matrix[i][j];
+        }
+      }
+    }
+  }
+
+  return count;
 }
 
-// console.log(matrixElementsSum(
-//     [[0, 1, 1, 2],
-//     [0, 5, 0, 0],
-//     [2, 0, 3, 3]]
-// ));
+console.log(
+  matrixElementsSum([
+    [0, 1, 1, 2],
+    [0, 5, 0, 0],
+    [2, 0, 3, 3],
+  ])
+);
