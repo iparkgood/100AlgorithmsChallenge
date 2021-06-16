56879878430ea90440a42cbbd5f951f11482213a
iparkgood <zuzu8723@gmail.com>
2021-06-16 12:35:56 -0400

    solved maxMultiple

--- maxMultiple/maxMultiple.ts
+++ maxMultiple/maxMultiple.ts
@@ -1,5 +1,5 @@
 export function maxMultiple(divisor: number, bound: number): number {
-
+  return bound - (bound % divisor);
 }
 
-// console.log(maxMultiple(3,10));
+console.log(maxMultiple(3, 10));
