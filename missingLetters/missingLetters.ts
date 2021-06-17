eb694715e99511bcadb197b55a2fe6bf130c1e8f
iparkgood <zuzu8723@gmail.com>
2021-06-17 13:54:25 -0400

    solved missingLetters

--- missingLetters/missingLetters.ts
+++ missingLetters/missingLetters.ts
@@ -1,8 +1,18 @@
 export function missingLetters(str: string): string {
+  if (str[0] !== "a") {
+    return "a";
+  }
 
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i + 1].charCodeAt(0) - str[i].charCodeAt(0) > 1) {
+      return String.fromCharCode(str[i].charCodeAt(0) + 1);
+    }
+  }
+
+  return undefined;
 }
 
-// console.log(missingLetters("bce"));
-// console.log(missingLetters("abce"));
-// console.log(missingLetters("abcdefghjklmno"));
-// console.log(missingLetters("abcdefghijklmnopqrstuvwxyz"));
+console.log(missingLetters("bce"));
+console.log(missingLetters("abce"));
+console.log(missingLetters("abcdefghjklmno"));
+console.log(missingLetters("abcdefghijklmnopqrstuvwxyz"));
