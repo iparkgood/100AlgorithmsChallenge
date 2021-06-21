e786c73b36e307ffb29c6570c13129fb52a9665b
iparkgood <zuzu8723@gmail.com>
2021-06-21 15:52:52 -0400

    solved newNumeralSystem

--- newNumeralSystem/newNumeralSystem.ts
+++ newNumeralSystem/newNumeralSystem.ts
@@ -1,5 +1,15 @@
 export function newNumeralSystem(number: string): string[] {
+  let result = [];
+  let g = number.charCodeAt(0) - 65;
 
+  for (let i = 0; i <= g; i++) {
+    result.push(
+      `${String.fromCharCode(i + 65)} + ${String.fromCharCode(g + 65)}`
+    );
+    g--;
+  }
+
+  return result;
 }
 
-// console.log(newNumeralSystem('G'));
+console.log(newNumeralSystem("G")); //["A + G", "B + F", "C + E", "D + D"]
