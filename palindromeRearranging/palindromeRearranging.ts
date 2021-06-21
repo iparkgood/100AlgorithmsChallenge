e5fff62dfc087fd077df08db0734a053db0443e3
iparkgood <zuzu8723@gmail.com>
2021-06-21 16:13:48 -0400

    solved palindromeRearranging

--- palindromeRearranging/palindromeRearranging.ts
+++ palindromeRearranging/palindromeRearranging.ts
@@ -1,5 +1,22 @@
 export function palindromeRearranging(inputString: string): boolean {
+  let obj = {};
+  let odd = 0;
 
+  for (let i = 0; i < inputString.length; i++) {
+    if (obj.hasOwnProperty(inputString[i])) {
+      obj[inputString[i]]++;
+    } else {
+      obj[inputString[i]] = 1;
+    }
+  }
+
+  for (const prop in obj) {
+    if (obj[prop] % 2 !== 0) {
+      odd++;
+    }
+  }
+
+  return odd === 0;
 }
 
-// console.log(palindromeRearranging('aabb'));
+console.log(palindromeRearranging("aabb"));
