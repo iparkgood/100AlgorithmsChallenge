13deddf4e02bae46f916767334446f13867d44dd
iparkgood <zuzu8723@gmail.com>
2021-06-23 10:05:45 -0400

    solved pigLatin

--- pigLatin/pigLatin.ts
+++ pigLatin/pigLatin.ts
@@ -1,6 +1,20 @@
 export function pigLatin(str: string): string {
-
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "i" ||
+      str[i] === "o" ||
+      str[i] === "u"
+    ) {
+      if (i === 0) {
+        return str + "way";
+      } else {
+        return str.slice(i, str.length) + str.slice(0, i) + "ay";
+      }
+    }
+  }
 }
 
-// console.log(pigLatin("glove"));
-// console.log(pigLatin("eight"));
+console.log(pigLatin("glove")); //oveglay
+console.log(pigLatin("eight")); //eightway
