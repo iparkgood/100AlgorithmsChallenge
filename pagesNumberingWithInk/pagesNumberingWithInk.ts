f488becde8a23e2bbbddd8887b7a61c765741c49
iparkgood <zuzu8723@gmail.com>
2021-06-23 10:06:13 -0400

    solved pagesNumberingWithInk

--- pagesNumberingWithInk/pagesNumberingWithInk.ts
+++ pagesNumberingWithInk/pagesNumberingWithInk.ts
@@ -1,5 +1,16 @@
-export function pagesNumberingWithInk(current: number, numberOfDigits: number): number {
+export function pagesNumberingWithInk(
+  current: number,
+  numberOfDigits: number
+): number {
+  while (current.toString().length <= numberOfDigits) {
+    numberOfDigits -= current.toString().length;
 
+    if (current.toString().length <= numberOfDigits) {
+      current++;
+    }
+  }
+
+  return current;
 }
 
-// console.log(pagesNumberingWithInk('G'));
+console.log(pagesNumberingWithInk(21, 5));
