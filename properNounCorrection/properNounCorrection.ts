cfc182195124749895bb5c64c4dd8d8ecbc51328
iparkgood <zuzu8723@gmail.com>
2021-06-29 18:33:42 -0400

    solved properNounCorrection

--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,6 +1,6 @@
 export function properNounCorrection(noun: string): string {
-
+  return noun.charAt(0).toUpperCase() + noun.slice(1).toLowerCase();
 }
 
-// console.log(properNounCorrection('pARiS'));
-// console.log(properNounCorrection('John'));
+console.log(properNounCorrection("pARiS"));
+console.log(properNounCorrection("John"));
