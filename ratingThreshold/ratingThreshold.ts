190aeeac3502793cd91d750788d64de04ceffce9
iparkgood <zuzu8723@gmail.com>
2021-06-29 19:41:39 -0400

    solved ratingThreshold

--- ratingThreshold/ratingThreshold.ts
+++ ratingThreshold/ratingThreshold.ts
@@ -1,8 +1,19 @@
 export function ratingThreshold(threshold: number, ratings: number[][]): number[] {
+  const ave = ratings.map(rating => {
+    const total = rating.reduce((acc, r) => {
+      return (acc + r);
+    }, 0)
+    return total / rating.length;
+  })
 
+  for(let i = 0 ; i < ave.length ; i++) {
+    if(ave[i] < threshold) {
+      return [i];
+    }
+  }
 }
 
-// console.log(ratingThreshold(3.5, 
-//     [[3, 4],
-//     [3, 3, 3, 4],
-//     [4]]));
+console.log(ratingThreshold(3.5, 
+    [[3, 4],
+    [3, 3, 3, 4],
+    [4]]));
