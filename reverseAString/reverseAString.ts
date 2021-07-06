ce243f4b74ff15662b65fcd7dc045bfcf319fb9f
iparkgood <zuzu8723@gmail.com>
2021-07-05 20:10:24 -0400

    solved reverseAString

--- reverseAString/reverseAString.ts
+++ reverseAString/reverseAString.ts
@@ -1,6 +1,6 @@
 export function reverseAString(str: string): string {
-
+  return [...str].reverse().join("");
 }
 
-// console.log(reverseAString('hello'));
-// console.log(reverseAString('Howdy'));
+console.log(reverseAString("hello"));
+console.log(reverseAString("Howdy"));
