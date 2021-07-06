a60e19e2965fe687a01b764012e21518c669e998
iparkgood <zuzu8723@gmail.com>
2021-07-06 13:22:16 -0400

    solved shapeArea

--- shapeArea/shapeArea.ts
+++ shapeArea/shapeArea.ts
@@ -1,6 +1,8 @@
 export function shapeArea(n: number): number {
+  if (n === 1) return 1;
 
+  return 1 + Math.pow(2, n);
 }
 
-// console.log(shapeArea(2));
-// console.log(shapeArea(3));
+console.log(shapeArea(2));
+console.log(shapeArea(3));
