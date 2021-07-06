f751b6884a47ffc74e9ee485b19f03e826aa3149
iparkgood <zuzu8723@gmail.com>
2021-07-05 20:07:51 -0400

    solved reflectString

--- reflectString/reflectString.ts
+++ reflectString/reflectString.ts
@@ -1,5 +1,34 @@
 export function reflectString(inputString: string): string {
+  const reflect = {
+    a: "z",
+    b: "y",
+    c: "x",
+    d: "w",
+    e: "v",
+    f: "u",
+    g: "t",
+    h: "s",
+    i: "r",
+    j: "q",
+    k: "p",
+    l: "o",
+    m: "n",
+    n: "m",
+    o: "l",
+    p: "k",
+    q: "j",
+    r: "i",
+    s: "h",
+    t: "g",
+    u: "f",
+    v: "e",
+    w: "d",
+    x: "c",
+    y: "b",
+    z: "a",
+  };
 
+  return [...inputString].map((input) => reflect[input]).join("");
 }
 
-// console.log(reflectString("name"));
+console.log(reflectString("name"));
