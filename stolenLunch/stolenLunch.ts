21b4a303d883837946de27e386b4a9871c202a22
iparkgood <zuzu8723@gmail.com>
2021-07-20 14:31:42 -0400

    solved stolenLunch

--- stolenLunch/stolenLunch.ts
+++ stolenLunch/stolenLunch.ts
@@ -1,5 +1,17 @@
 export function stolenLunch(note: string): string {
+  let numToChar = [...note.slice(0, note.indexOf(":"))];
+  let charToNum = [...note.slice(note.indexOf(":") + 2)]
+    .map((x) => x.charCodeAt(0) - 97)
+    .join("");
 
+  for (let i = 0; i < numToChar.length; i++) {
+    if (Number.isInteger(+numToChar[i]) && numToChar[i] !== " ") {
+      const code = String.fromCharCode(+numToChar[i] + 97);
+      numToChar[i] = code;
+    }
+  }
+
+  return numToChar.join("") + ": " + charToNum;
 }
 
-// console.log(stolenLunch("you'll n4v4r 6u4ss 8t: cdja"));
+console.log(stolenLunch("you'll n4v4r 6u4ss 8t: cdja"));
