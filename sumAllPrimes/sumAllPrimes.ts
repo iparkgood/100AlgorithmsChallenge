386a0e1d4b8d2c5d7da9d9d8a622b727b23c371d
iparkgood <zuzu8723@gmail.com>
2021-07-22 12:05:33 -0400

    solved sumAllPrimes

--- sumAllPrimes/sumAllPrimes.ts
+++ sumAllPrimes/sumAllPrimes.ts
@@ -1,6 +1,22 @@
 export function sumAllPrimes(num: number): number {
+  let sumPrimes = 0;
 
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+      sumPrimes += i;
+    }
+  }
+
+  return sumPrimes;
 }
 
-// console.log(sumAllPrimes(10));
-// console.log(sumAllPrimes(977));
+function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+}
+
+console.log(sumAllPrimes(10));
+console.log(sumAllPrimes(977));
