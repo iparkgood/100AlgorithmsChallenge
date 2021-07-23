29d0921cac30189145c97b71f9adab638ee701c1
iparkgood <zuzu8723@gmail.com>
2021-07-23 12:11:26 -0400

    solved sumOddFibonacciNums

--- sumOddFibonacciNums/sumOddFibonacciNums.ts
+++ sumOddFibonacciNums/sumOddFibonacciNums.ts
@@ -1,7 +1,15 @@
 export function sumOddFibonacciNums(num: number): number {
+  let fibs = [1, 1];
+  let idx = 2;
 
+  while (fibs[idx - 2] + fibs[idx - 1] < num) {
+    fibs.push(fibs[idx - 2] + fibs[idx - 1]);
+    idx++;
+  }
+
+  return fibs.filter((f) => f % 2 !== 0).reduce((acc, n) => acc + n);
 }
 
-// console.log(sumOddFibonacciNums(1000));
-// console.log(sumOddFibonacciNums(4000000));
-// console.log(sumOddFibonacciNums(10));
+console.log(sumOddFibonacciNums(1000));
+console.log(sumOddFibonacciNums(4000000));
+console.log(sumOddFibonacciNums(10));
