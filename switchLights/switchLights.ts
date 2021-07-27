0dd844750e8b257367094a7e53775cbe631a67f5
iparkgood <zuzu8723@gmail.com>
2021-07-27 15:15:14 -0400

    solved switchLights

--- switchLights/switchLights.ts
+++ switchLights/switchLights.ts
@@ -1,6 +1,20 @@
 export function switchLights(a: number[]): number[] {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === 1) {
+      a = swapLights(a, i);
+    }
+  }
 
+  return a;
 }
 
-// console.log(switchLights([1, 1, 1, 1, 1]));
-// console.log(switchLights([0, 0]));
+function swapLights(lights, idx) {
+  for (let i = 0; i < idx + 1; i++) {
+    lights[i] = lights[i] === 1 ? 0 : 1;
+  }
+
+  return lights;
+}
+
+console.log(switchLights([1, 1, 1, 1, 1]));
+console.log(switchLights([0, 0]));
