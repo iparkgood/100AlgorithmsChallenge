9582a6775ffe8d717ba3d707c41134b3d486f5e5
iparkgood <zuzu8723@gmail.com>
2021-07-30 11:18:04 -0400

    solved sumOfTwo

--- sumOfTwo/sumOfTwo.ts
+++ sumOfTwo/sumOfTwo.ts
@@ -1,5 +1,19 @@
 export function sumOfTwo(a: number[], b: number[], v: number): boolean {
+  let obj = {};
 
+  for (let i = 0; i < a.length; i++) {
+    if (!obj.hasOwnProperty(a[i])) {
+      obj[a[i]] = 1;
+    }
+  }
+
+  for (let i = 0; i < b.length; i++) {
+    if (v - b[i] in obj) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
-// console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
+console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
