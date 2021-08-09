590b4d42a57ac79e142ff4a95b24c4a18ed81bbb
iparkgood <zuzu8723@gmail.com>
2021-08-09 16:17:21 -0400

    solved uniqueDigitProducts

--- uniqueDigitProducts/uniqueDigitsProducts.ts
+++ uniqueDigitProducts/uniqueDigitsProducts.ts
@@ -2,18 +2,12 @@ export function uniqueDigitProducts(a: number[]): number {
   let result = [];
 
   a.forEach((number) => {
-    if (number > 9) {
-      const productResult = product(number);
-      if (!result.includes(productResult)) {
-        result.push(productResult);
-      }
-    } else {
-      if (!result.includes(number)) {
-        result.push(number);
-      }
+    const productResult = product(number);
+    if (!result.includes(productResult)) {
+      result.push(productResult);
     }
   });
-  console.log(result);
+
   return result.length;
 }
 
