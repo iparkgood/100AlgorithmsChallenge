7e01c0461a043812de68917acd64532dc22884df
iparkgood <zuzu8723@gmail.com>
2021-08-10 12:51:45 -0400

    solved validTime

--- validTime/validTime.ts
+++ validTime/validTime.ts
@@ -1,7 +1,10 @@
 export function validTime(time: string): boolean {
+  const hour = time.slice(0, time.indexOf(":"));
+  const minute = time.slice(time.indexOf(":") + 1);
 
+  return +hour < 24 && +hour > 0 && +minute < 60 && +minute > 0;
 }
 
-// console.log(validTime('13:58'));
-// console.log(validTime('25:51'));
-// console.log(validTime('02:76'));
+console.log(validTime("13:58"));
+console.log(validTime("25:51"));
+console.log(validTime("02:76"));
